90751d15710c55ba12df11813e8c0df08d6a1256
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-26 16:47:27 +0900

    Notepad: Add comments

--- notepad.js
+++ notepad.js
@@ -3,12 +3,18 @@ goog.provide('tutorial.notepad.Note');
 goog.require('goog.dom');
 goog.require('goog.ui.Zippy');
 
+/**
+ * Manages the data and interface for a single note.
+ */
 tutorial.notepad.Note = function(title, content, noteContainer) {
   this.title = title;
   this.content = content;
   this.parent = noteContainer;
 };
 
+/**
+ * Creates the DOM structure for the note and adds it to the document.
+ */
 tutorial.notepad.Note.prototype.makeNoteDom = function() {
   // Create DOM structure to represent the note.
   this.headerElement = goog.dom.createDom(
@@ -23,6 +29,10 @@ tutorial.notepad.Note.prototype.makeNoteDom = function() {
   return new goog.ui.Zippy(this.headerElement, this.contentElement);
 };
 
+/**
+ * Iterates over a list of note data objects, creates a Note instance
+ * for each one, and tells the instance to build its DOM structure.
+ */
 tutorial.notepad.makeNotes = function(data, noteContainer) {
   var notes = [];
   for (var i = 0; i < data.length; i++) {
